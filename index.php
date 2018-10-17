46c271315a0326ab34eff5ce481c52d197ff5ae8
laykith <laykith@gmail.com>
2018-10-17 13:05:42 +0200

    Fixed require paths

--- index.php
+++ index.php
@@ -3,21 +3,20 @@
 declare(strict_types=1);
 
 //Logic files
-require __DIR__.'app/functions.php';
-require __DIR__.'database/dbConnect.php';
+require __DIR__.'/app/functions.php';
+require __DIR__.'/database/dbConnect.php';
 $database = new dbConnect();
 
 //Build array of data
-$query = 'SELECT * FROM :post ORDER BY post.creationdate LIMIT 25';
+$query = 'SELECT * FROM post ORDER BY post.creationdate LIMIT 25';
 $params = [];
 $data = $database->getData($query, $params);
 
 //View
-require __DIR__.'views/header.php';
+require __DIR__.'/views/header.php';
 
 echo '<pre>';
 print_r($data);
-
 echo '</pre>';
 
-require __DIR__.'views/footer.php';
+require __DIR__.'/views/footer.php';
