3a10276088f292b4690d028d3ab9fb47948b6097
laykith <laykith@gmail.com>
2018-10-17 13:40:04 +0200

    Added Post component

--- index.php
+++ index.php
@@ -3,19 +3,20 @@
 declare(strict_types=1);
 
 //Logic files
-require __DIR__.'/app/functions.php';
 require __DIR__.'/database/dbConnect.php';
 $database = new dbConnect();
 
 //Build array of data
 $query = 'SELECT * FROM posts LIMIT 25';
-$data = $database->getData($query, $params);
+$posts = $database->getData($query, $params);
 
 //View
 require __DIR__.'/views/header.php';
 
-echo '<pre>';
-print_r($data);
-echo '</pre>';
+if (isset($posts)) {
+    require __DIR__.'/views/components/posts.php';
+} else {
+    echo 'An error has occured';
+}
 
 require __DIR__.'/views/footer.php';
