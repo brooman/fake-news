29d7c2bb4f068f4bfeb79f55aa6b7b711b2d6e20
laykith <laykith@gmail.com>
2018-10-17 15:13:27 +0200

    Added login and register form components

---
+++ views/components/loginform.php
@@ -0,0 +1,14 @@
+<form method="post">
+    <fieldset>
+        <legend>Login</legend>
+        <div class="form-group">
+            <label for="username">Email address</label>
+            <input type="text" class="form-control" id="username" name="username" placeholder="Enter username">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </fieldset>
+</form>
---
+++ views/components/registerform.php
@@ -0,0 +1,18 @@
+<form method="post">
+    <fieldset>
+        <legend>Login</legend>
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Hello, what's your name?">
+        </div>
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" class="form-control" id="username" name="username" placeholder="Enter username">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </fieldset>
+</form>
