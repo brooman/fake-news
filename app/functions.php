d9efb3049b174ca9f0c7cecf3b1c99d1c42c169b
laykith <laykith@gmail.com>
2018-10-17 12:54:47 +0200

    Changed how we pull data from database

--- app/functions.php
+++ app/functions.php
@@ -1,14 +1,3 @@
 <?php
 
 declare(strict_types=1);
-/**
- * Get posts from database.
- *
- * @return array
- */
-function getData(): array
-{
-    $data = [];
-
-    return $data;
-}
--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 
 declare(strict_types=1);
 
+//Logic files
 require __DIR__.'app/functions.php';
+require __DIR__.'database/dbConnect.php';
+$database = new dbConnect();
 
-$data = getData();
+//Build array of data
+$query = 'SELECT * FROM :post ORDER BY post.creationdate LIMIT 25';
+$params = [];
+$data = $database->getData($query, $params);
 
 //View
 require __DIR__.'views/header.php';
+
+echo '<pre>';
+print_r($data);
+
+echo '</pre>';
 
 require __DIR__.'views/footer.php';
