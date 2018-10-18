05e2e7d8fa2c61156e00491ca28844418f875b94
laykith <laykith@gmail.com>
2018-10-18 11:06:55 +0200

    Updated create script

--- database/sql_source/create.sql
+++ database/sql_source/create.sql
@@ -1,19 +1,24 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2018-10-18 08:02:26.078
+
+-- tables
+-- Table: posts
+CREATE TABLE posts (
+    id integer NOT NULL CONSTRAINT posts_pk PRIMARY KEY,
+    user_id integer NOT NULL,
+    title varchar(256) NOT NULL,
+    content text NOT NULL,
+    creationdate datetime NOT NULL,
+    CONSTRAINT posts_user FOREIGN KEY (user_id)
+    REFERENCES user (id)
+);
+
+-- Table: user
 CREATE TABLE user (
     id integer NOT NULL CONSTRAINT user_pk PRIMARY KEY AUTOINCREMENT,
     name varchar(64) NOT NULL,
     username varchar(64) NOT NULL,
     password varchar(256) NOT NULL
-);
-
--- Table: posts
-CREATE TABLE posts (
-    id integer NOT NULL CONSTRAINT posts_pk PRIMARY KEY AUTOINCREMENT,
-    user_id integer NOT NULL,
-    title varchar(256) NOT NULL,
-    content text NOT NULL,
-    creationdate date NOT NULL,
-    CONSTRAINT posts_author FOREIGN KEY (user_id)
-    REFERENCES user (id)
 );
 
 -- End of file.
