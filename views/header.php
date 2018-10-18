daa6baff487dd93098fb742285a668ce3140f638
laykith <laykith@gmail.com>
2018-10-18 11:30:43 +0200

    Added hrefs in navbar

--- views/header.php
+++ views/header.php
@@ -22,15 +22,18 @@
         <div class="collapse navbar-collapse" id="main-navbar">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link" href="../index.php">Home</a>
                 </li>
                 <?php if (!$_SESSION['loggedIn']): ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Log in</a>
+                    <a class="nav-link" href="../admin/login.php">Log in</a>
                 </li>
                 <?php else: ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Log out</a>
+                    <a class="nav-link" href="../admin/panel.php">Admin Panel</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../admin/logout.php">Log out</a>
                 </li>
                 <?php endif; ?>
             </ul>
