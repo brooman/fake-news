673d7ba367a050c14a1260cf86bea0aff6f9cb7c
laykith <laykith@gmail.com>
2018-10-18 11:35:56 +0200

    Centered login and register form

--- views/components/registerform.php
+++ views/components/registerform.php
@@ -1,30 +1,32 @@
-<div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
-    <div class="card-header">Register</div>
-    <div class="card-body">
-        <form method="post">
-            <div class="form-group row">
-                <div class="col-sm-12">
-                    <label for="name">Name</label>
-                    <input id="name" name="name" type="text" class="form-control here" required="required">
+<div class="row justify-content-center mt-3">
+    <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+        <div class="card-header">Register</div>
+        <div class="card-body">
+            <form method="post">
+                <div class="form-group row">
+                    <div class="col-sm-12">
+                        <label for="name">Name</label>
+                        <input id="name" name="name" type="text" class="form-control here" required="required">
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-sm-12">
-                    <label for="username">Username</label>
-                    <input id="username" name="username" type="text" class="form-control here" required="required">
+                <div class="form-group row">
+                    <div class="col-sm-12">
+                        <label for="username">Username</label>
+                        <input id="username" name="username" type="text" class="form-control here" required="required">
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-sm-12">
-                    <label for="Password">Password</label>
-                    <input id="Password" name="password" type="password" class="form-control here" required="required">
+                <div class="form-group row">
+                    <div class="col-sm-12">
+                        <label for="Password">Password</label>
+                        <input id="Password" name="password" type="password" class="form-control here" required="required">
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-sm-12">
-                    <button name="submit" type="submit" class="btn btn-success">Submit</button>
+                <div class="form-group row">
+                    <div class="col-sm-12">
+                        <button name="submit" type="submit" class="btn btn-success">Submit</button>
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 </div>
