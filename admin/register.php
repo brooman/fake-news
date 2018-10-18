34fbb3afc1086adf9bc4fb2d39336529c537c459
laykith <laykith@gmail.com>
2018-10-18 12:45:24 +0200

    Added validation to prevent 2 users from having same username

--- admin/register.php
+++ admin/register.php
@@ -4,26 +4,36 @@ declare(strict_types=1);
 
 session_start();
 
-//Check login
+//Check if post data is submitted
 if (isset($_POST['username'])) {
     require $_SERVER['DOCUMENT_ROOT'].'/database/dbConnect.php';
     $database = new dbConnect();
 
-    //Get password from DB
-    $query = 'INSERT INTO user (id, name, username, password) VALUES (NULL, :name, :username, :password)';
+    //Check if username is already taken
+    $validationQuery = 'SELECT user.username FROM user WHERE user.username = :username';
     $params = [
-        ':name' => $_POST['name'],
         ':username' => $_POST['username'],
-        ':password' => password_hash($_POST['password'], PASSWORD_BCRYPT),
     ];
-    $database->setData($query, $params);
 
-    header('location: login.php');
+    //If not add user & redirect to login page
+    if (!empty($database->getData($validationQuery, $params))) {
+
+        //Create user
+        $insertQuery = 'INSERT INTO user (id, name, username, password) VALUES (NULL, :name, :username, :password)';
+        $params = [
+        ':name' => strip_tags($_POST['name']),
+        ':username' => strip_tags($_POST['username']),
+        ':password' => password_hash($_POST['password'], PASSWORD_BCRYPT),
+        ];
+        $database->setData($insertQuery, $params);
+
+        //Redirect to login
+        header('location: login.php');
+    }
 }
 
 //View
 require $_SERVER['DOCUMENT_ROOT'].'/views/header.php';
-
 require $_SERVER['DOCUMENT_ROOT'].'/views/components/registerform.php';
 
 require $_SERVER['DOCUMENT_ROOT'].'/views/footer.php';
