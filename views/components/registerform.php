3b16de4fdee5ea67f63b0d10b0cfb0f4e193141f
laykith <laykith@gmail.com>
2018-10-18 09:17:20 +0200

    Removed 3 more classes that missed last commit. Whoops

--- views/components/registerform.php
+++ views/components/registerform.php
@@ -9,19 +9,19 @@
                 </div>
             </div>
             <div class="form-group row">
-                <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12">
                     <label for="username">Username</label>
                     <input id="username" name="username" type="text" class="form-control here" required="required">
                 </div>
             </div>
             <div class="form-group row">
-                <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12">
                     <label for="Password">Password</label>
                     <input id="Password" name="Password" type="text" class="form-control here" required="required">
                 </div>
             </div>
             <div class="form-group row">
-                <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12">
                     <button name="submit" type="submit" class="btn btn-success">Submit</button>
                 </div>
             </div>
