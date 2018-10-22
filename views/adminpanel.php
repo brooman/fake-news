2a4dc239a0d34933d730cf2fdced87af6fcf7761
laykith <laykith@gmail.com>
2018-10-22 10:27:11 +0200

    Added basic admin panel html for making posts

--- views/adminpanel.php
+++ views/adminpanel.php
@@ -1,3 +1,20 @@
-<div class="row">
-
+<div class="container">
+    <h1 class="text-white">New Post</h1>
+    <div class="row">
+        <div class="col-sm-12">
+            <form>
+                <div class="form-group">
+                    <label for="post_title">Title</label>
+                    <input id="post_title" name="post_title" type="text" required="required" class="form-control here">
+                </div>
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <textarea id="content" name="content" cols="40" rows="20" class="form-control" required="required"></textarea>
+                </div>
+                <div class="form-group">
+                    <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
