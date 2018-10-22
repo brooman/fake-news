7b25b548f856d8ca026f57c6e053b9d228c7b189
laykith <laykith@gmail.com>
2018-10-22 10:44:44 +0200

    Updated class names

--- views/adminpanel.php
+++ views/adminpanel.php
@@ -2,14 +2,14 @@
     <h1 class="text-white">New Post</h1>
     <div class="row">
         <div class="col-sm-12">
-            <form>
+            <form method='post'>
                 <div class="form-group">
                     <label for="post_title">Title</label>
                     <input id="post_title" name="post_title" type="text" required="required" class="form-control here">
                 </div>
                 <div class="form-group">
-                    <label for="content">Content</label>
-                    <textarea id="content" name="content" cols="40" rows="20" class="form-control" required="required"></textarea>
+                    <label for="post_content">Content</label>
+                    <textarea id="post_content" name="post_content" cols="40" rows="20" class="form-control" required="required"></textarea>
                 </div>
                 <div class="form-group">
                     <button name="submit" type="submit" class="btn btn-primary">Submit</button>
