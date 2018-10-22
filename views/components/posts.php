8621d5df182ce8c687e2f82704cd8bd3fdf8a959
laykith <laykith@gmail.com>
2018-10-22 11:19:03 +0200

    Made posts prettier

--- views/components/posts.php
+++ views/components/posts.php
@@ -1,16 +1,18 @@
-<?php foreach ($posts as $post): ?>
-<div class="jumbotron">
-    <h2 class="display-3">
-        <?= $post['title']; ?>
-    </h2>
-    <p>
-        Author
-    </p>
-    <p class="lead">
-        <?= $post['content']; ?>
-    </p>
-    <p>
-        <?= $post['creationdate']; ?>
-    </p>
+<div class="container">
+    <?php foreach ($posts as $post): ?>
+    <div class="jumbotron">
+        <h2 class="display-3">
+            <?= $post['title']; ?>
+        </h2>
+        <p>
+            Author
+        </p>
+        <p class="lead">
+            <?= $post['content']; ?>
+        </p>
+        <p>
+            <?= $post['creationdate']; ?>
+        </p>
+    </div>
+    <?php endforeach; ?>
 </div>
-<?php endforeach;
