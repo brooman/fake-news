0de6457fa096aebb3f527815bc3dd75dd2bc3d70
laykith <laykith@gmail.com>
2018-10-26 22:52:47 +0200

    Added security to prevent file stealers >:(

---
+++ error/403.php
@@ -0,0 +1,14 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'].'/views/header.php';
+?>
+<div class="row justify-content-center">
+    <div class="card text-white bg-danger mb-3" style="width: 30rem">
+        <div class="card-body">
+            <h4 class="card-title">403 Forbidden</h4>
+            <p class="card-text">Seems like you tried to access something you were not supposed to see.</p>
+        </div>
+        <a href="../index.php" class="btn btn-primary">Take me home</a>
+    </div>
+</div>
+<?php
+require $_SERVER['DOCUMENT_ROOT'].'/views/footer.php';
