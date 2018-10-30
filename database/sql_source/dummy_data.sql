72295fbfaea692164c5b42ce5a625c00c2724304
laykith <laykith@gmail.com>
2018-10-30 11:17:02 +0100

    Added dummy data

---
+++ database/sql_source/dummy_data.sql
@@ -0,0 +1,95 @@
+--Names by
+--https://www.fantasynamegen.com/demon/medium/
+
+--Post data by 
+--http://www.crazyclientipsum.com/
+
+
+--Users
+INSERT INTO user (name, username, password) VALUES ('Jabachlo', 'demon1', 'password1');
+INSERT INTO user (name, username, password) VALUES ('Ko-as', 'demon2', 'password1');
+INSERT INTO user (name, username, password) VALUES ('Baelsiascasca', 'demon3', 'password1');
+INSERT INTO user (name, username, password) VALUES ('Pizeah', 'demon4', 'password1');
+INSERT INTO user (name, username, password) VALUES ('Chiciasson', 'demon5', 'password1');
+
+
+--Posts
+INSERT INTO posts (user_id, title, content, creationdate) VALUES (1, 'This site is bad', 
+'Can you move it over half a pixel to the right? Looks good, but... The font looks too fonty. It''s too blocky. There''s too much white space. I''m not sure about the colour that you''ve used for the background, I guess it looks OK on screen but when I print it on our office printer it kinda reminds me of a pair of corduroys I had as a child. It''s too blocky.
+
+Why doesn''t this work in IE6? I think the internet is down. Can you use a "clear" background on my website? No, not white. Transparent. The text would definitely look better in Comic Sans. Pay attention to message "above the fold," the "fold" itself, and, everything else that automatically falls below the fold. The design needs to be less liney. There are too many lines. None of our hyperlinks are working. We don''t know where it''s trying to link to. We just underlined the text and were not further prompted.
+
+It needs to be more edgy. We think it would be better to stick with darker bright colors. The government blocked javascript or something... That''s why my Yahoo mail isn''t coming onto my phone. I''ll know it when I see it. Can we add transitions like in PowerPoint? '
+, ABS(RANDOM()) % (9999999999 - 1000000000) + 1000000000); -- Random timestamp
+
+INSERT INTO posts (user_id, title, content, creationdate) VALUES (1, 'Your design sucks', 
+'Could you make the design "pop" a bit more? The white isn''t too white. Overall perhaps we could somehow exchange some color for dimension/depth/texture? And by dimension/depth/texture I just mean "3d-ness" of the boxes or the buttons. We''re just not seeing that "ka-pow" factor. The government blocked javascript or something... That''s why my Yahoo mail isn''t coming onto my phone. I don''t like it. If you could work in some subtle opacity and/or glass Apple-style buttons in the design it would be great. For some reason I just have a warm fuzzy feeling about both those things.
+
+Overall perhaps we could somehow exchange some color for dimension/depth/texture? And by dimension/depth/texture I just mean "3d-ness" of the boxes or the buttons. We think it would be better to stick with darker bright colors. I like it but can you change it? Hey, I know it''s been a while, but do you happen to know our office WiFi password? I don''t like it. Can you please rename this file as [filename]? Thanks! Then send it back to me so I can send it off. I did this at home with Microsoft Paint. We need to jazz it up a little.
+
+I like it but can you change it? Looks good, but... The government blocked javascript or something... That''s why my Yahoo mail isn''t coming onto my phone. The text would definitely look better in Comic Sans. How do I click that? None of our hyperlinks are working. We don''t know where it''s trying to link to. We just underlined the text and were not further prompted.'
+, ABS(RANDOM()) % (9999999999 - 1000000000) + 1000000000);
+
+INSERT INTO posts (user_id, title, content, creationdate) VALUES (2, 'Support for older browsers???', 
+'How come the site does not work in IE7? It is 2014 and you should be able to get it to work in that by now. I am very disappointed. Okay, yeah I think the word program I used to design that doesn''t have the strongest pixels. Overall perhaps we could somehow exchange some color for dimension/depth/texture? And by dimension/depth/texture I just mean "3d-ness" of the boxes or the buttons. I think the internet is down. We may want to put a sunrise on it. We''re just not seeing that "ka-pow" factor. Can you make the logo bigger?
+
+Can you move it over half a pixel to the right? Okay, yeah I think the word program I used to design that doesn''t have the strongest pixels. We want it to look very "Web 2.0". Pay attention to message "above the fold," the "fold" itself, and, everything else that automatically falls below the fold. It''s too blocky. Can we just start and I''ll figure out the details as we go? I think the internet is down. Can we just start and I''ll figure out the details as we go?
+
+How do I click that? Pay attention to message "above the fold," the "fold" itself, and, everything else that automatically falls below the fold. We want it to look very "Web 2.0". Can we do the website in black and white to save some money? I know that Papyrus font is considered old-fashioned, but I have to say, we find the font easy on the eye and ultimately easy to read. It''s all about professionalism. Why doesn''t this work in IE6? '
+, ABS(RANDOM()) % (9999999999 - 1000000000) + 1000000000);
+
+INSERT INTO posts (user_id, title, content, creationdate) VALUES (2, 'I don''t like you', 
+'How do I click that? None of our hyperlinks are working. We don''t know where it''s trying to link to. We just underlined the text and were not further prompted. I''ll know it when I see it. Can we do the website in black and white to save some money? Can we put more ads on the page? None of our hyperlinks are working. We don''t know where it''s trying to link to. We just underlined the text and were not further prompted. Not sure if it''s possible, but a rotating "flash" photo would be pretty awesome in the top nav area. I really like the color of the image when I tilt my laptop display all the way down. Please make that the normal look of the image.
+
+Instead of the boring image of a "hand" when a client clicks on something, please make it a small (but friendly) garden insect - e.g. a butterfly, grasshopper, or hummingbird. Please avoid insects with negative connotations, e.g. a slug, caterpillar, or hawk. I like it but can you change it? Can you make the logo bigger? I can''t say why, exactly, but the illustration of the dinosaur doesn''t seem realistic enough. Can we use a photo instead? Can we put more ads on the page? Can you make the logo bigger? Could you make the design "pop" a bit more? Looks good, but...
+
+I don''t like it. I like it but can you change it? I like the banner but you need to change the font to something that makes me smile. Obviously, this one is not making me smile. In fact, it''s upsetting me. There''s too much white space. The design needs to be less liney. There are too many lines. Looks good, but... We were hoping to see some more design in the design. Let''s get a QR code of our website and add it on our website. '
+, ABS(RANDOM()) % (9999999999 - 1000000000) + 1000000000);
+
+INSERT INTO posts (user_id, title, content, creationdate) VALUES (3, 'Needed Improvments', 
+'Can you please rename this file as [filename]? Thanks! Then send it back to me so I can send it off. I''ll know it when I see it. I''m not sure about the colour that you''ve used for the background, I guess it looks OK on screen but when I print it on our office printer it kinda reminds me of a pair of corduroys I had as a child. Instead of the boring image of a "hand" when a client clicks on something, please make it a small (but friendly) garden insect - e.g. a butterfly, grasshopper, or hummingbird. Please avoid insects with negative connotations, e.g. a slug, caterpillar, or hawk. Overall perhaps we could somehow exchange some color for dimension/depth/texture? And by dimension/depth/texture I just mean "3d-ness" of the boxes or the buttons. It needs to be more edgy. Not sure if it''s possible, but a rotating "flash" photo would be pretty awesome in the top nav area. Can you make it so when people click on their start-bar my website pops up?
+
+I really like the color of the image when I tilt my laptop display all the way down. Please make that the normal look of the image. Can we add transitions like in PowerPoint? Looks good, but... I did this at home with Microsoft Paint. The text would definitely look better in Comic Sans. Let''s get a QR code of our website and add it on our website. Can we just start and I''ll figure out the details as we go?
+
+I can''t say why, exactly, but the illustration of the dinosaur doesn''t seem realistic enough. Can we use a photo instead? I think the internet is down. Look, I''ve been using the internet since the 90''s... So I could probably design this site myself. You''re more like a second opinion. I really like the color of the image when I tilt my laptop display all the way down. Please make that the normal look of the image. This is great, but let''s take it to the next level. Can you make it so when people click on their start-bar my website pops up? '
+, ABS(RANDOM()) % (9999999999 - 1000000000) + 1000000000);
+
+INSERT INTO posts (user_id, title, content, creationdate) VALUES (3, 'Printer problems', 
+'The white isn''t too white. Can we do the website in black and white to save some money? Overall perhaps we could somehow exchange some color for dimension/depth/texture? And by dimension/depth/texture I just mean "3d-ness" of the boxes or the buttons. Looks good, but... We were hoping to see some more design in the design. The design needs to be less liney. There are too many lines. Looks good, but... Make the logo bigger. I want them to know what website they''re on. Hey, I know it''s been a while, but do you happen to know our office WiFi password?
+
+Make the logo bigger. I want them to know what website they''re on. The white isn''t too white. How come the site does not work in IE7? It is 2014 and you should be able to get it to work in that by now. I am very disappointed. I like the banner but you need to change the font to something that makes me smile. Obviously, this one is not making me smile. In fact, it''s upsetting me. Youtube does it, why can''t you build it just like them? Pay attention to message "above the fold," the "fold" itself, and, everything else that automatically falls below the fold. I did this at home with Microsoft Paint.
+
+I''m not sure about the colour that you''ve used for the background, I guess it looks OK on screen but when I print it on our office printer it kinda reminds me of a pair of corduroys I had as a child. Looks good, but... I can''t say why, exactly, but the illustration of the dinosaur doesn''t seem realistic enough. Can we use a photo instead? '
+, ABS(RANDOM()) % (9999999999 - 1000000000) + 1000000000);
+
+INSERT INTO posts (user_id, title, content, creationdate) VALUES (4, 'Microsoft paint has better design than you.', 
+'The font looks too fonty. We want to maintain the appearance of credibility. I did this at home with Microsoft Paint. Not sure if it''s possible, but a rotating "flash" photo would be pretty awesome in the top nav area. Looks good, but... I like it but can you change it? I really like the color of the image when I tilt my laptop display all the way down. Please make that the normal look of the image. We''re just not seeing that "ka-pow" factor. I know that Papyrus font is considered old-fashioned, but I have to say, we find the font easy on the eye and ultimately easy to read. It''s all about professionalism.
+
+I''ll know it when I see it. I don''t like circles. How come the site does not work in IE7? It is 2014 and you should be able to get it to work in that by now. I am very disappointed. I can''t say why, exactly, but the illustration of the dinosaur doesn''t seem realistic enough. Can we use a photo instead? If you could work in some subtle opacity and/or glass Apple-style buttons in the design it would be great. For some reason I just have a warm fuzzy feeling about both those things. Can you make the logo bigger? How come the site does not work in IE7? It is 2014 and you should be able to get it to work in that by now. I am very disappointed. I know that Papyrus font is considered old-fashioned, but I have to say, we find the font easy on the eye and ultimately easy to read. It''s all about professionalism.
+
+We want it to look very "Web 2.0". We need to jazz it up a little. I think the internet is down. I''m not sure about the colour that you''ve used for the background, I guess it looks OK on screen but when I print it on our office printer it kinda reminds me of a pair of corduroys I had as a child. Can you make it so when people click on their start-bar my website pops up? We may want to put a sunrise on it. I don''t like it. I think the internet is down. '
+, ABS(RANDOM()) % (9999999999 - 1000000000) + 1000000000);
+
+INSERT INTO posts (user_id, title, content, creationdate) VALUES (4, 'Stop using modern fonts', 
+'I know that Papyrus font is considered old-fashioned, but I have to say, we find the font easy on the eye and ultimately easy to read. It''s all about professionalism. I like the banner but you need to change the font to something that makes me smile. Obviously, this one is not making me smile. In fact, it''s upsetting me. We think it would be better to stick with darker bright colors. The design needs to be less liney. There are too many lines. Looks good, but... The white isn''t too white.
+
+Overall perhaps we could somehow exchange some color for dimension/depth/texture? And by dimension/depth/texture I just mean "3d-ness" of the boxes or the buttons. Instead of the boring image of a "hand" when a client clicks on something, please make it a small (but friendly) garden insect - e.g. a butterfly, grasshopper, or hummingbird. Please avoid insects with negative connotations, e.g. a slug, caterpillar, or hawk. Looks good, but... I''m not sure about the colour that you''ve used for the background, I guess it looks OK on screen but when I print it on our office printer it kinda reminds me of a pair of corduroys I had as a child. There''s too much white space.
+
+It''s too blocky. We want to maintain the appearance of credibility. I''m not sure about the colour that you''ve used for the background, I guess it looks OK on screen but when I print it on our office printer it kinda reminds me of a pair of corduroys I had as a child. I like it but can you change it? Looks good, but... Well I don''t like the pages who has loading when you get to the website, I don''t want to have to wait. Let''s get a QR code of our website and add it on our website. '
+, ABS(RANDOM()) % (9999999999 - 1000000000) + 1000000000);
+
+INSERT INTO posts (user_id, title, content, creationdate) VALUES (5, 'Please support my website', 
+'I really like the color of the image when I tilt my laptop display all the way down. Please make that the normal look of the image. None of our hyperlinks are working. We don''t know where it''s trying to link to. We just underlined the text and were not further prompted. We think it would be better to stick with darker bright colors. We want it to look very "Web 2.0". We may want to put a sunrise on it. Hey, I know it''s been a while, but do you happen to know our office WiFi password? How does this color look on your monitor? Is the ''chocolate'' looking more like the burgundy wine that one drinks while eating the chocolate?
+
+I don''t like it. Can you make it so when people click on their start-bar my website pops up? Not sure if it''s possible, but a rotating "flash" photo would be pretty awesome in the top nav area. Looks good, but... We need to jazz it up a little. The font looks too fonty. How come the site does not work in IE7? It is 2014 and you should be able to get it to work in that by now. I am very disappointed. I think the internet is down.
+
+I really like the color of the image when I tilt my laptop display all the way down. Please make that the normal look of the image. We''re just not seeing that "ka-pow" factor. How do I click that? Can we just start and I''ll figure out the details as we go? The text would definitely look better in Comic Sans. Look, I''ve been using the internet since the 90''s... So I could probably design this site myself. You''re more like a second opinion. Well I don''t like the pages who has loading when you get to the website, I don''t want to have to wait. Can you use a "clear" background on my website? No, not white. Transparent. 
+', ABS(RANDOM()) % (9999999999 - 1000000000) + 1000000000);
+
+INSERT INTO posts (user_id, title, content, creationdate) VALUES (5, 'QR Codes is hot right now', 
+'Pay attention to message "above the fold," the "fold" itself, and, everything else that automatically falls below the fold. Hey, I know it''s been a while, but do you happen to know our office WiFi password? I think the internet is down. I know that Papyrus font is considered old-fashioned, but I have to say, we find the font easy on the eye and ultimately easy to read. It''s all about professionalism.
+
+Let''s get a QR code of our website and add it on our website. Hey, I know it''s been a while, but do you happen to know our office WiFi password? Instead of the boring image of a "hand" when a client clicks on something, please make it a small (but friendly) garden insect - e.g. a butterfly, grasshopper, or hummingbird. Please avoid insects with negative connotations, e.g. a slug, caterpillar, or hawk. I really like the color of the image when I tilt my laptop display all the way down. Please make that the normal look of the image. I''ll know it when I see it. Could you make the design "pop" a bit more? I know that Papyrus font is considered old-fashioned, but I have to say, we find the font easy on the eye and ultimately easy to read. It''s all about professionalism. Look, I''ve been using the internet since the 90''s... So I could probably design this site myself. You''re more like a second opinion. I''ll know it when I see it.
+
+Can you please rename this file as [filename]? Thanks! Then send it back to me so I can send it off. I really like the color of the image when I tilt my laptop display all the way down. Please make that the normal look of the image. The font looks too fonty. Can you make the logo bigger? I know that Papyrus font is considered old-fashioned, but I have to say, we find the font easy on the eye and ultimately easy to read. It''s all about professionalism. Can we do the website in black and white to save some money? Why doesn''t this work in IE6? Looks good, but... Can you make it so when people click on their start-bar my website pops up? '
+, ABS(RANDOM()) % (9999999999 - 1000000000) + 1000000000);
